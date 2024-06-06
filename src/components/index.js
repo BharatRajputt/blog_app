fa521172883477290d5ed75b91e03f50869233df
BharatRajputt <you@example.com>
2024-06-06 20:27:28 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,32 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import RTE from "./RTE";
+import Signup from "./SignUp";
+import Login from "./Login";
+import PostForm from './post-folder/PostForm'
+import PostCard from './PostCard'
+import Post from '../pages/Post'
+import  AuthLayout from './AuthLayout'
+import Button from './Button'
+import Input from './Input'
+import Select from './Select'
+export{
+    Header,
+    Footer,
+    Container,
+    Logo,
+    LogoutBtn,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard,
+    Post,
+    AuthLayout,
+    Button,
+    Input,
+    Select
+}
